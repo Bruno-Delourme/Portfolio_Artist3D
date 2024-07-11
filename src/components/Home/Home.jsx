68504a3b3af5c38f4f67bd47f9a6dd60a6415085
Bruno-Delourme <delourmebruno@gmail.com>
2024-07-11 19:06:14 +0200

    modif style on neon

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <section className="home">
       <div className="home-content">
-        <h1>3D ARTIST <p></p>GAME DESIGNER</h1>
+        <h1>3D ARTIST <p></p>GAME ARTIST</h1>
         <p>Je suis toujours prêt à me dépasser pour créer des visuels qui accentuent l&apos;immersivité des projets sur lesquels je travaille.</p>
         
         <button className="about-button">
