8f919ea1a0a1cb24dd040e38eb85c17d1ec8db35
Bruno-Delourme <delourmebruno@gmail.com>
2024-07-12 16:36:26 +0200

    header scroll top

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,50 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Header = () => {
+  const [showHeader, setShowHeader] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlHeader = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > lastScrollY) { 
+        // Scrolling down
+        setShowHeader(false);
+      } else { 
+        // Scrolling up
+        setShowHeader(true);
+      }
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlHeader);
+
+      return () => {
+        window.removeEventListener('scroll', controlHeader);
+      };
+    }
+  }, [lastScrollY]);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+
   return (
-    <header className="header">
-      <div className="logo">MACEO </div>
+    <header className={`header ${showHeader ? 'show' : 'hide'}`}>
+      <div className="logo" onClick={scrollToTop}>MACEO</div>
       <nav>
         <ul className="nav-links">
           <li><a href="#">PROJETS</a></li>
           <li><a href="#">A PROPOS</a></li>
           <li><a href="#">CONTACT</a></li>
-          
         </ul>
       </nav>
     </header>
   );
-}
+};
 
 export default Header;
