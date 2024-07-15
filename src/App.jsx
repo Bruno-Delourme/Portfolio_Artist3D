03b8cfa7e4023c660e9505e0f5d99feacf33b407
Bruno-Delourme <delourmebruno@gmail.com>
2024-07-15 15:24:52 +0200

    portfolio start 3d artist

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Contact from './components/Contact/Contact';
 import MagnifyingGlass from './components/MagnifyingGlass/MagnifyingGlass';
+import Object3DViewer from './components/Objet3d/Objet3D';
+
 import './styles.css';
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
     <div className="App">
       <Header />
       <Home />
+      <Object3DViewer />
       <MagnifyingGlass text="Ceci est la phrase à agrandir sous la loupe." />
       <Contact />
     </div>
--- src/components/Objet3d/Objet3D.jsx
+++ src/components/Objet3d/Objet3D.jsx
@@ -2,13 +2,17 @@
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { useLoader } from '@react-three/fiber';
 import PropTypes from 'prop-types';
 
 const Model = ({ path }) => {
-  const gltf = useLoader(GLTFLoader, path);
-  return <primitive object={gltf.scene} />;
+  const obj = useLoader(OBJLoader, path);
+
+  // Ajout pour vérifier le chargement du modèle
+  console.log(obj);
+
+  return <primitive object={obj} />;
 };
 
 Model.propTypes = {
@@ -19,7 +23,9 @@ const Object3DViewer = () => {
   return (
     <Canvas style={{ height: '100vh', width: '100vw' }}>
       <Suspense fallback={null}>
-        <Model path="/objet3d/your-model-file.gltf" />
+        <ambientLight intensity={0.5} />  {/* Ajout d'une lumière ambiante */}
+        <directionalLight position={[0, 5, 5]} intensity={1} />  {/* Ajout d'une lumière directionnelle */}
+        <Model path="/objet3d/Procedural_Eyes.Obj.obj" />  {/* Chemin vers le modèle 3D */}
         <OrbitControls />
       </Suspense>
     </Canvas>
